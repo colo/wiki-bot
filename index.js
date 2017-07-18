eccf2c5106d60b6e21f6852e5bfb604835691de8
Luciano Bueno <glamero@gmail.com>
2017-07-18 16:51:53 -0300

    bump

--- index.js
+++ index.js
@@ -1,37 +1,137 @@
 //https://github.com/Fannon/mwbot
 
-const MWBot = require('mwbot');
+const MWBot = require('mwbot'),
+			Moo = require("mootools"),
+			fs = require('fs');
 
-let bot = new MWBot({
-	apiUrl: 'http://api-250c157acc41b2668b36bb7a241bd300.sandstorm.e-ducativa.x:6080/api.php',
-	verbose: true,
-	jar: true,
-	json: true
-});
+var wikitiva = require('./config/wikitiva.js'),
+		sandstorm = require('./config/sandstorm.js');
 
-bot.setGlobalRequestOptions({
-    qs: {
-        format: 'json'
-    },
-    headers: {
-        'Authorization': 'Bearer xtXcsCD_zuqCMBXq1pOzZt23CnDWK0BnzzJGVdN43gJ'
-    },
-    timeout: 120000, // 120 seconds
-    jar: true,
-    time: true,
-    json: true
-})
 
-bot.getEditToken().then((response) => {
+let wikitiva_bot = new MWBot(wikitiva.config);
+let sandstorm_bot = new MWBot(sandstorm.config);
+
+sandstorm_bot.setGlobalRequestOptions(sandstorm.request)
+
+
+wikitiva_bot.login({
+	username: wikitiva.credentials.username,
+	password: wikitiva.credentials.password
+}).then((response) => {
 	console.log(response);
-	return bot.edit('Servicios Rabbitmq', 'Test Content3', 'Test Summary');
+	
+	Array.each(wikitiva.pages, function(page){
+		
+		read(page, function(text){
+			//console.log(text);
+			write(page, text, function(resp){
+				console.log(resp);
+			});
+		});
+		
+	})
+	
 })
-.then((response) => {
-	console.log(response);
-    // Success
-}).catch((err) => {
+.catch((err) => {
 	console.log(err);
     // Error
 });
 
+function write(page, text, callback){
+	
+	fs.stat('./docs/'+page, function(err, stat) {
+		
+		if(err == null) {
+			console.log('WRITE File: '+page+' exists');
+			write_to_wiki(page, text, callback);
+		}
+		else{
+			console.log('WRITE File: '+page+' DOESN\'T exists');
+			
+			var wstream = fs.createWriteStream('./docs/'+page);
+			wstream.on('finish', function () {
+				write_to_wiki(page, text, callback);
+			})
+			.on('error', function (error) {
+				//console.log(error);
+				callback(error);
+			})
+			
+			wstream.write(text);
+		}
+		
+	});
+	
+};
 
+function write_to_wiki(page, text, callback){
+	page = page.replace(/_/g, ' ');
+	
+	page = page.replace('Infra Servidores Externos ', '');
+	//console.log(page);
+	
+	text = text.replace(/Infra_Servidores_Externos_/g, '');
+	//console.log(text);
+	
+	sandstorm_bot.getEditToken().then((response) => {
+		//console.log(response);
+		return sandstorm_bot.edit(page, text);
+	})
+	.then((response) => {
+		//console.log(response);
+		callback(response);
+	}).catch((err) => {
+		callback(err);
+	});
+
+};
+	
+function read(page, callback){
+	fs.stat('./docs/'+page, function(err, stat) {
+		
+		if(err == null) {
+			console.log('File: '+page+' exists');
+			var data = '';
+
+			var readStream = fs.createReadStream('./docs/'+page, 'utf8');
+
+			readStream.on('data', function(chunk) {  
+					data += chunk;
+			})
+			.on('error', function(error) {
+				read_from_wiki(page, callback);
+			})
+			.on('end', function() {
+				//console.log(data);
+				callback(data);
+			});
+		}
+		else{
+			console.log('File: '+page+' DOESN\'T exists');
+			read_from_wiki(page, callback);
+		}
+		//else if(err.code == 'ENOENT') {
+			//// file does not exist
+			////fs.writeFile('log.txt', 'Some log\n');
+			//console.log('File DOESN\'T exists');
+		//}
+		//else {
+			//console.log('Some other error: ', err.code);
+		//}
+		
+	});
+};
+
+function read_from_wiki(page, callback){
+	wikitiva_bot.read(page).then((response) => {
+		console.log(response);
+		var page = Object.keys(response.query.pages)[0];
+		//console.log(response.query.pages[page]['revisions'][0]['*']);
+			// Success
+		callback(response.query.pages[page]['revisions'][0]['*']);
+	})
+	.catch((err) => {
+		console.log(err);
+			// Error
+	});
+};
